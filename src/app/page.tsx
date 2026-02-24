03a8f260c81fb711c0fe9ccc40041fc98289e0b4
Freddy-bij <freddybijanja31@gmail.com>
2026-02-24 20:56:48 +0200

    chcdn installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 export default function Home() {
   return (
-    <div>welcome to our website.</div>
+    <div className="bg-[#4F46E5] h-screen text-[#111827] border border-[#D9D9D9]">home page</div>
   );
 }
