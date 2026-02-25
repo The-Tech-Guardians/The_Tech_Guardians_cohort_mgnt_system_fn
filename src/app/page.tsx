54d7bd515cc5ab95a640d432ff84efff636ad3ba
Freddy-bij <freddybijanja31@gmail.com>
2026-02-25 07:10:21 +0200

    remove bacground

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "../components/ui/button";
 
 export default function Home() {
   return (
-    <div className="bg-[#4F46E5] h-screen text-[#111827] border border-[#D9D9D9]">home page</div>
+    <div >
+      home page
+      </div>
   );
 }
