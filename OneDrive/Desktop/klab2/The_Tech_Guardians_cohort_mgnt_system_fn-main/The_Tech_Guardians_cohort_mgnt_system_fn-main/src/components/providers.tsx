36f1724224ae2c66d51b1a815c74edb94fb89201
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2026-03-17 10:59:11 +0100

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/klab2/The_Tech_Guardians_cohort_mgnt_system_fn-main/The_Tech_Guardians_cohort_mgnt_system_fn-main/src/components/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { AuthProvider } from '@/lib/useAuth';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
